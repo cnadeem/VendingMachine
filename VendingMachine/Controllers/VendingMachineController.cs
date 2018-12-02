0851543ff4d278ed8fd5722a1a98020cd9b0f6e6
Nadeem Choudhary <cnadeemahmed@hotmail.com>
2018-12-02 13:14:18 +1100

    initial

---
+++ VendingMachine/Controllers/VendingMachineController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VendingMachine.Controllers
+{
+    public class VendingMachineController : Controller
+    {
+        // GET: VendingMachine
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
